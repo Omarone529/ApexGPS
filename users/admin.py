42c76630631f433375d0d70d965821684f32b972
Omar <omarlucky0@gmail.com>
2025-11-30 10:25:59 +0100

    feat: add role managment in the admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    """
+    Customize the display of the CustomUser template in the Admin panel.
+    """
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('role',)}),
+    )
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('ApexGPS Role', {'fields': ('role',)}),
+    )
+
+    list_display = UserAdmin.list_display + ('role',)
+
+    list_filter = UserAdmin.list_filter + ('role',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
