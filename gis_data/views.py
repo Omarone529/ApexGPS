959ec079c16dc4e0fc6d516fc8fc563e0d9ee17b
Omar <omarlucky0@gmail.com>
2025-12-07 18:12:31 +0100

    feat: add middleware permissions

--- gis_data/views.py
+++ gis_data/views.py
@@ -1,10 +1,33 @@
 from rest_framework import viewsets
 
+from users.permissions import IsAdminUser, IsRegisteredUser
+
 from .models import PointOfInterest, ScenicArea
 from .serializers import PointOfInterestSerializer, ScenicAreaSerializer
 
 
-class PointOfInterestViewSet(viewsets.ModelViewSet):
+class ProtectedResourceViewSet(viewsets.ModelViewSet):
+    """
+    Base ViewSet for protected resources with role-based permissions.
+
+    Permission rules:
+    - Anyone can view (GET)
+    - Only registered users can create (POST)
+    - Only administrators can update or delete (PUT, PATCH, DELETE)
+    """
+
+    def get_permissions(self):
+        """Return a list of permissions needed to access the view."""
+        if self.action in ["list", "retrieve"]:
+            return []
+        elif self.action == "create":
+            return [IsRegisteredUser()]
+        elif self.action in ["update", "partial_update", "destroy"]:
+            return [IsAdminUser()]
+        return []
+
+
+class PointOfInterestViewSet(ProtectedResourceViewSet):
     """
     ViewSet for PointOfInterest model.
     Provides CRUD operations for points of interest.
@@ -14,7 +37,7 @@ class PointOfInterestViewSet(viewsets.ModelViewSet):
     serializer_class = PointOfInterestSerializer
 
 
-class ScenicAreaViewSet(viewsets.ModelViewSet):
+class ScenicAreaViewSet(ProtectedResourceViewSet):
     """
     ViewSet for ScenicArea model.
     Provides CRUD operations for scenic areas.
